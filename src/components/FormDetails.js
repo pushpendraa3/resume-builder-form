7a13f938cf9d3917a20e94ab6495fb86748e70fb
Pushpendra Singh Bhadoriya <pushpendrasingh.91650@gmail.com>
2023-04-09 10:17:43 +0530

    set up basic form

---
+++ src/components/FormDetails.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SimpleAccordion from './SimpleAccordion'
+
+export default function FormDetails() {
+  return (
+    <div>
+      <SimpleAccordion />
+    </div>
+  )
+}
---
+++ src/components/SimpleAccordion.js
@@ -0,0 +1,87 @@
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import './SimpleAccordion.css';
+import { useForm } from "react-hook-form";
+
+
+export default function SimpleAccordion() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = data => console.log(data);
+  // console.log(watch("example")); // watch input value by passing the name of it
+
+  return (
+    <div className='accordian-box'>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Basic Details</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+
+              <input {...register("firstName", { required: true })} />
+              {errors.firstName && <span>First Name is required</span>}
+
+              <input {...register("lastName", { required: true })} />
+              {errors.lastName && <span>Last Name is required</span>}
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Employment Summary</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Education</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Other Details</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <input type="submit" />
+    </form>
+    </div>
+  );
+}
